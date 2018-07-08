030c769f8eaa685d7c3381fa5588123c2dadf096
qaisaru@icloud.com <qaisaru@icloud.com>
2018-07-08 20:31:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
-  var array = []
+  var array = [];
   for (var i = 0; i < musicians.length; i++) {
     array.push(musicians[i] + ' plays ' + instruments[i]);
   }
-  return array
+  return array;
 }
