38a4b98669e9be80d86600affb3a6e2ea4247ac1
qaisaru@icloud.com <qaisaru@icloud.com>
2018-07-08 20:53:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,5 +27,9 @@ function johnLennonFacts(facts){
 function iLoveTheBeatles(num){
   var array = [];
   do 
+    { array.push("I love the Beatles!")
+      num++;
+  } while (num < 15);
+  return array;
 }
 
